b09ccd50466fa01898b29fa418f7737a91b0219e
nathandrake3652 <131317866+nathandrake3652@users.noreply.github.com>
2025-05-12 17:14:16 -0400

    Home The home page is real
    https://www.youtube.com/watch?v=gWo12TtN9Kk&pp=ygUWY2FuIHdlIGdldCBtdWNoIGhpZ2hlcg%3D%3D

--- Frontend/my-app/src/hooks/useLogin.ts
+++ Frontend/my-app/src/hooks/useLogin.ts
@@ -7,21 +7,18 @@ interface Logindata {
     password: string;
 }
 
-interface Loginresponse {
-    token: string;
-   
-}
+
 
 export function useLogin(onSuccess: (token: string)=> void, onFail:(error:string)=> void) {
-    return useMutation<Loginresponse,AxiosError,Logindata>({
-        mutationFn: async ({rut,password}: Logindata): Promise<Loginresponse> => {
+    return useMutation<string,AxiosError,Logindata>({
+        mutationFn: async ({rut,password}: Logindata): Promise<string> => {
             const respuesta = await api.post('api/v1/auth/Login', {rut,password});
             console.log(respuesta.data);
             return respuesta.data;
         },
         onSuccess: (data) => {
             console.log('Login exitoso', data);
-            onSuccess(data.token);
+            onSuccess(data);
         },
         onError:(error) => {
             const mensaje = (error.response?.data as {message?: string})?.message || 'no se pudo identificar el error xd';
