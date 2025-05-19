eec27124ff80c791d5d8b0e7d0a70ad9a22fa9f0
BarcobasureroBG <benjamin.garces@alumnos.ucn.cl>
2025-05-18 20:40:06 -0400

    github anda chistoso

--- Frontend/my-app/src/pages/Reservar.tsx
+++ Frontend/my-app/src/pages/Reservar.tsx
@@ -2,22 +2,30 @@ import React, {SyntheticEvent, useState} from 'react'
 import { useUserProfile } from '../hooks/useUserProfile';
 import {useNavigate} from "react-router-dom";
 import { useAuth } from '../context/AuthContext';
-import { useReservasGenerales,crearReserva } from '../hooks/useReservas';
-import {agregarSaldo} from '../hooks/useUsuarios';
+import { useReservasGenerales,useCrearReserva } from '../hooks/useReservas';
+import {useAgregarSaldo} from '../hooks/useUsuarios';
 
 export const Reservar = () => {
     const {token, setToken} = useAuth();
     const navigate = useNavigate();
     const {data: user, isLoading: cargauser, isError} = useUserProfile();
     const {data: reservas,isLoading: cargaReserva} = useReservasGenerales();
-    const crearReserv = crearReserva();
+    const crearReserv = useCrearReserva();
     const [rut_cliente, setRut] = useState('');
     const [fecha,setFecha] = useState('');
     const [hora_inicio, setHoraInicio] = useState('');
-    const [hora_fin,setHoraFin] = useState('')
-    const [id_cancha,setIDCancha] = useState('')
-    const [boleta_equipamiento,setBoleta] = useState('')
+    const [hora_fin,setHoraFin] = useState('');
+    const [id_cancha,setIDCancha] = useState('');
+    const [boleta_equipamiento,setBoleta] = useState('');
     
+    const [saldoAgregado,setSaldo] = useState('');
+    const agregarMonto = useAgregarSaldo();
+
+    const enviar = (e: SyntheticEvent) => {
+        e.preventDefault();
+        agregarMonto.mutate(Number(saldoAgregado));
+      };
+
     /*const crear = (e: SyntheticEvent) => {
         e.preventDefault();
         crearProd.mutate({name: nombre, price: Number(precio), description: descripcion, method: metodo});
@@ -48,6 +56,7 @@ export const Reservar = () => {
 
     return (
     <div> 
+        
         <h2> Bienvenido: {user?.rut}, tu saldo actual es: ${user?.saldo} </h2>
         
 
